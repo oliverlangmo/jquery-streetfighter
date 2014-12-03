5ab66c08b881a40c32dc9e97de8c1e4bfeffdf91
oliverlangmo <oliver.langmo@gmail.com>
2014-12-02 18:10:17 -0600

    hadouken animated/gone

--- js/app.js
+++ js/app.js
@@ -1,23 +1,34 @@
 $(document).ready(function() {
-$('.ryu').mouseenter(function() {
+  $('.ryu').mouseenter(function() {
     $('.ryu-still').hide();
     $('.ryu-ready').show();
   })
-.mouseleave(function() {
+  .mouseleave(function() {
     $('.ryu-ready').hide();
     $('.ryu-still').show();
   })
-.mousedown(function() {
-    console.log('mousedown');
-    // play hadouken sound
-    $('.ryu-ready').hide();
-    $('.ryu-throwing').show();
-    $('.hadouken').show();
-    // show hadouken and animate it to the right of the screen
-  })
+  .mousedown(function() {
+  playHadouken();    
+  $('.ryu-ready').hide();
+  $('.ryu-throwing').show();
+  $('.hadouken').finish().show()
+  .animate(
+    {'left': '300px'},
+    500,
+    function() {
+      $(this).hide();
+      $(this).css('left', '-212px');
+    }
+  );
+})
   .mouseup(function() {
     $('.ryu-throwing').hide();
     $('.ryu-ready').show();
-    // ryu goes back to his ready position
   });
 });
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
