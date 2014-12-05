56b55b061fae0c3ba7d3a134ee857aabe6a291c5
oliverlangmo <oliver.langmo@gmail.com>
2014-12-04 20:34:32 -0600

    jquery working

--- js/app.js
+++ js/app.js
@@ -25,10 +25,25 @@ $(document).ready(function() {
     $('.ryu-throwing').hide();
     $('.ryu-ready').show();
   });
-});
+  
+  
+  $(document).keydown(function(e){
+  	if (e.keyCode == 88) {
+  		$(".ryu-cool").show();
+  		$(".ryu-ready").hide();
+  	}
+  }).keyup(function(e) {
+  	if (e.keyCode == 88) {
+  		$('.ryu-ready').show();
+  		$('.ryu-cool').hide();
+  	}
+  }
+ );
+ 
 
 function playHadouken () {
   $('#hadouken-sound')[0].volume = 0.5;
   $('#hadouken-sound')[0].load();
   $('#hadouken-sound')[0].play();
 }
+});
